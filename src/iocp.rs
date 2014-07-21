c7c0b7a2b2a468b01b9d152fea62332e3240872c
Peter Atashian <retep998@gmail.com>
2014-07-21 15:30:31 -0400

    Added file writing.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/iocp.rs
+++ src/iocp.rs
@@ -11,6 +11,7 @@ mod iocp {
     type BOOL = libc::c_int;
     type DWORD = libc::c_ulong;
     type HANDLE = *mut VOID;
+    type LPCVOID = *const VOID;
     type LPCWSTR = *const WCHAR;
     type LPDWORD = *mut DWORD;
     type LPOVERLAPPED = *mut OVERLAPPED;
@@ -20,11 +21,13 @@ mod iocp {
     type ULONG_PTR = uint;
     type VOID = libc::c_void;
     type WCHAR = u16;
+    static CREATE_NEW: DWORD = 1;
     static ERROR_HANDLE_EOF: DWORD = 38;
     static ERROR_IO_PENDING: DWORD = 997;
     static FILE_FLAG_OVERLAPPED: DWORD = 0x40000000;
     static FILE_SHARE_READ: DWORD = 0x00000001;
     static GENERIC_READ: DWORD = 0x80000000;
+    static GENERIC_WRITE: DWORD = 0x40000000;
     static INFINITE: DWORD = 0xFFFFFFFF;
     static INVALID_HANDLE_VALUE: HANDLE = -1 as HANDLE;
     static OPEN_EXISTING: DWORD = 3;
@@ -59,6 +62,13 @@ mod iocp {
             lpNumberOfBytesRead: LPDWORD,
             lpOverlapped: LPOVERLAPPED,
         ) -> BOOL;
+        fn WriteFile(
+            hFile: HANDLE,
+            lpBuffer: LPCVOID,
+            nNumberOfBytesToWrite: DWORD,
+            lpNumberOfBytesWritten: LPDWORD,
+            lpOverlapped: LPOVERLAPPED,
+        ) -> BOOL;
     }
     struct SECURITY_ATTRIBUTES;
     #[deriving(Show)]
@@ -88,9 +98,9 @@ mod iocp {
         pos: u64,
     }
     impl File {
-        pub fn new(name: &str) -> Result<File, &'static str> {
+        pub fn open(name: &Path) -> IoResult<File> {
             let handle = unsafe { CreateFileW(
-                utf16(name).as_ptr(),
+                utf16(name.as_str().unwrap()).as_ptr(),
                 GENERIC_READ,
                 FILE_SHARE_READ,
                 mut_null(),
@@ -99,13 +109,38 @@ mod iocp {
                 mut_null(),
             ) };
             if handle == INVALID_HANDLE_VALUE {
-                return Err("Failed to open file");
+                return Err(IoError::last_error());
             }
             let port = unsafe {
                 CreateIoCompletionPort(handle, mut_null(), 0, 0)
             };
             if port == mut_null() {
-                return Err("Failed to create IO Completion Port");
+                return Err(IoError::last_error());
+            }
+            Ok(File {
+                handle: handle,
+                port: port,
+                pos: 0,
+            })
+        }
+        pub fn create(name: &Path) -> IoResult<File> {
+            let handle = unsafe { CreateFileW(
+                utf16(name.as_str().unwrap()).as_ptr(),
+                GENERIC_WRITE,
+                FILE_SHARE_READ,
+                mut_null(),
+                CREATE_NEW,
+                FILE_FLAG_OVERLAPPED,
+                mut_null(),
+            ) };
+            if handle == INVALID_HANDLE_VALUE {
+                return Err(IoError::last_error());
+            }
+            let port = unsafe {
+                CreateIoCompletionPort(handle, mut_null(), 0, 0)
+            };
+            if port == mut_null() {
+                return Err(IoError::last_error());
             }
             Ok(File {
                 handle: handle,
@@ -120,7 +155,7 @@ mod iocp {
             if unsafe { ReadFile(
                 self.handle,
                 buf.as_mut_ptr() as LPVOID,
-                buf.len() as u32,
+                buf.len() as DWORD,
                 mut_null(),
                 &mut over as LPOVERLAPPED,
             ) } == 0 {
@@ -152,13 +187,54 @@ mod iocp {
             Ok(bytes as uint)
         }
     }
+    impl Writer for File {
+        fn write(&mut self, buf: &[u8]) -> IoResult<()> {
+            let mut over = OVERLAPPED::new(self.pos);
+            if unsafe { WriteFile(
+                self.handle,
+                buf.as_ptr() as LPCVOID,
+                buf.len() as DWORD,
+                mut_null(),
+                &mut over as LPOVERLAPPED,
+            ) } == 0 {
+                match unsafe { GetLastError() } {
+                    ERROR_IO_PENDING => (),
+                    err => return Err(IoError::from_errno(err as uint, true)),
+                }
+            }
+            let mut bytes = unsafe { uninitialized() };
+            let mut key = unsafe { uninitialized() };
+            let mut over = unsafe { uninitialized() };
+            if unsafe { GetQueuedCompletionStatus(
+                self.port,
+                &mut bytes as LPDWORD,
+                &mut key as PULONG_PTR,
+                &mut over as *mut LPOVERLAPPED,
+                INFINITE,
+            ) } == 0 {
+                match unsafe { GetLastError() } {
+                    ERROR_HANDLE_EOF => return Err(IoError {
+                        kind: EndOfFile,
+                        desc: "end of file",
+                        detail: None,
+                    }),
+                    err => return Err(IoError::from_errno(err as uint, true)),
+                }
+            }
+            if buf.len() != bytes as uint {
+                fail!("ow");
+            }
+            Ok(())
+        }
+    }
     fn utf16(s: &str) -> Vec<u16> {
         s.utf16_units().collect::<Vec<u16>>().append_one(0)
     }
 }
 
 fn main() {
-    let mut file = iocp::File::new("test.txt").unwrap();
+    let mut file = iocp::File::open(&Path::new("../src/iocp.rs")).unwrap();
     let s = file.read_to_string().unwrap();
-    println!("{}", s);
+    let mut file = iocp::File::create(&Path::new("dump.txt")).unwrap();
+    file.write_str(s.as_slice()).unwrap();
 }
