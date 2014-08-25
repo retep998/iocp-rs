93a9e2af6a3ff84ec7ba415e1c554fe76d0b0393
Peter Atashian <retep998@gmail.com>
2014-08-25 03:12:37 -0400

    Fix test issue.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- tests/test.rs
+++ tests/test.rs
@@ -1,10 +1,12 @@
 
+#![reexport_test_harness_main = "test_main"]
+
 extern crate iocp;
 extern crate green;
 
 #[start]
 fn start(argc: int, argv: *const *const u8) -> int {
-    green::start(argc, argv, iocp::event_loop, __test::main)
+    green::start(argc, argv, iocp::event_loop,test_main)
 }
 
 #[test]
